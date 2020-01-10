d8d32bda2e8ad08d3bcacc5662f9ab43b65776b9
Alpha Olomi <alphaolomi@gmail.com>
2020-01-11 02:13:16 +0300

    add jar assemble

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,4 +3,8 @@ public class Hello {
     public String sayHello() {
         return "Hello, world!";
     }
+
+    public static void main(String[] args) {
+        System.out.println("I can say hello from the console!");
+    }
 }
